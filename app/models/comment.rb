13a62f2b31a840a57b83d7b46a55040fdd4aa65d
kusi <lawcubegsm@gmail.com>
2022-12-01 10:55:21 +0000

    Add comment methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  def update_post_counter
+    post.update(comments_counter: author.posts_counter + 1)
+  end
+
+
 end
