3df9131a89ac18097602495bed193530a06c2857
kusi <lawcubegsm@gmail.com>
2022-12-01 10:36:25 +0000

    Add post methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  def update_posts_counter
+    author.update(posts_counter: author.posts_counter + 1)
+  end
+
+  def retrieve_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
- # def get_recent_posts
- #     self.posts.order(:created_at).limit(3)
- # end
+  def get_recent_posts
+     self.posts.order(:created_at).limit(3)
+  end
 end
