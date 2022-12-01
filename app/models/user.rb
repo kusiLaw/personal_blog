2a8eb02e52bd382ec023a042145791b47ebdc70d
kusi <lawcubegsm@gmail.com>
2022-12-01 10:54:55 +0000

    Add user methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  def get_recent_posts
-     self.posts.order(:created_at).limit(3)
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
   end
 end
