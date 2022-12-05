147d7a9d747aae8011d53b81872b31bd254625e3
kusi <lawcubegsm@gmail.com>
2022-12-05 20:20:53 +0000

    Create user controllers
    - set as index routes
    - add user details views in show
    - add user index html
    - add show user html

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+  root 'users#index'
+  
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
