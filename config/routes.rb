42974bc33c0b31853b0a39517986387c6a206e1f
kusi <lawcubegsm@gmail.com>
2022-12-05 21:00:32 +0000

    Add association links between posts and users

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   get 'posts/index'
   # get 'users/index'
   root 'users#index'
-  
-  resources :users
+
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
