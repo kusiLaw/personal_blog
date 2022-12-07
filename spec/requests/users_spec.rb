6247fa20694a443350b1f36b6a98b2fbb199d7b2
kusi <lawcubegsm@gmail.com>
2022-12-07 09:04:28 +0000

    Fix linter error

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -19,20 +19,19 @@ RSpec.describe 'Users', type: :request do
   end
 
   describe 'GET #show' do
-   before { get '/users/5' } 
- 
-   it 'returns http success' do
-     expect(response).to have_http_status(:ok)
-   end
- 
-   it 'render index template' do
-     expect(response).to render_template('show')
-   end
- 
-   it 'rendered index include text' do
-     expect(response).to render_template('show')
-     expect(response.body).to include('Here is a details of a given user')
-   end
-  end
+    before { get '/users/5' }
 
+    it 'returns http success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'render index template' do
+      expect(response).to render_template('show')
+    end
+
+    it 'rendered index include text' do
+      expect(response).to render_template('show')
+      expect(response.body).to include('Here is a details of a given user')
+    end
+  end
 end
