991701794c967a068347742acb396a7f84e7fd79
kusi <lawcubegsm@gmail.com>
2022-12-19 13:29:43 +0000

    Fix yaml linter error

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,8 +39,8 @@ Capybara.javascript_driver = :selenium_chrome
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  # rubocop:disable Metrics/BlockLength
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.fixture_path = "#{Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
