a96b0c4578709a6161594262048d73938f797d21
kusi <lawcubegsm@gmail.com>
2022-12-19 12:38:23 +0000

    Add post test
    - add test to post details
    - add test user posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,6 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
-    # print @post.user, 'iiiiuyu'
   end
 
   def new
