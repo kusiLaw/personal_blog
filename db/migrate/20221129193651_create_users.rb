6096b23dea0bc6ecdc4386d5dfac8aedd6a86c15
kusi <lawcubegsm@gmail.com>
2022-12-21 13:11:50 +0000

    Add role to user

--- db/migrate/20221129193651_create_users.rb
+++ db/migrate/20221129193651_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :photo, default: 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png'
       t.string :bio
       t.integer :posts_counter, default: 0
-
+      
       t.timestamps
     end
   end
